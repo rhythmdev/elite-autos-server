11401a1f5451b3a1afbc5116a5daa5e4fcd16417
rhythmdev <rhythmbiswas@gmail.com>
2023-12-05 17:05:26 +0600

    product create api making done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hjmkubl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +28,23 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+         
+        const productCollections = client.db('EliteAutos').collection('products');
+
+
+        //create a product
+        app.post('/products', async(req, res) =>{
+            const newProduct = req.body;
+            const result = await productCollections.insertOne(newProduct);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
