12b100d7a7d5eabbf4228adc54212502b8a336a6
rhythmdev <rhythmbiswas@gmail.com>
2023-12-09 00:30:44 +0600

    fixed issue

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const productCollections = client.db('EliteAutos').collection('products');
         const brandCollection = client.db('EliteAutos').collection('brands');
